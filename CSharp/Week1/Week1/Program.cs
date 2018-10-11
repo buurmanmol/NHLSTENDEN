3fdb7f437344881a0605be8a79e3d5d1585f43be
Jeroen Hulshof <jeroenhulshof@me.com>
2018-10-11 18:54:37 +0200

    Added Language selection

--- CSharp/Week1/Week1/Program.cs
+++ CSharp/Week1/Week1/Program.cs
@@ -45,43 +45,92 @@ namespace Week1
 
             //Console.Clear();
 
-            Console.WriteLine("Bent u een man of een vrouw?");
-
-            String sex = Console.ReadLine();
-            sex = sex.ToUpper();
+            Console.WriteLine("What is the language you're speaking? (Dutch or English): ");
+            String chosenLanguage = Console.ReadLine();
+            chosenLanguage = chosenLanguage.ToUpper();
 
             int lengthInCm;
             int circumference;
             int acceptedSex = 0;
 
-            while (acceptedSex != 1)
-            {
-                if (sex == "MAN")
-                {
-                    acceptedSex++;
-                    Console.WriteLine("Hoe lang bent u? (in centimeters): ");
-                    lengthInCm = Convert.ToInt32(Console.ReadLine());
-                    var calculationMan = (lengthInCm - 100) * 0.9;
-                    Console.WriteLine("Het ideale lichaamsgewicht voor u is: " + calculationMan);
+            String sex = Console.ReadLine();
 
-                }
-                else if (sex == "VROUW")
-                {
-                    acceptedSex++;
-                    Console.WriteLine("Hoe lang bent u? (in centimeters): ");
-                    lengthInCm = Convert.ToInt32(Console.ReadLine());
-                    Console.WriteLine("Wat is uw pols omtrek? (in centimeters):  ");
-                    circumference = Convert.ToInt32(Console.ReadLine());
-                    var calculationVrouw = (lengthInCm + 4 * circumference - 100) / 2;
-                    Console.WriteLine("Het ideale lichaamsgewicht voor u is: " + calculationVrouw);
-                }
-                else {
-                    Console.WriteLine("U moet kiezen uit Man of Vrouw. Probeer het opnieuw: ");
+            switch (chosenLanguage)
+            {
+                case "DUTCH":
+
+                    Console.WriteLine("Bent u een man of een vrouw");
                     sex = Console.ReadLine();
                     sex = sex.ToUpper();
-                }
-            }
 
+                    while (acceptedSex != 1)
+                    {
+                        if (sex == "MAN")
+                        {
+                            acceptedSex++;
+                            Console.WriteLine("Hoe lang bent u? (in centimeters): ");
+                            lengthInCm = Convert.ToInt32(Console.ReadLine());
+                            var calculationMan = (lengthInCm - 100) * 0.9;
+                            Console.WriteLine("Het ideale lichaamsgewicht voor u is: " + calculationMan);
+
+                        }
+                        else if (sex == "VROUW")
+                        {
+                            acceptedSex++;
+                            Console.WriteLine("Hoe lang bent u? (in centimeters): ");
+                            lengthInCm = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Wat is uw pols omtrek? (in centimeters):  ");
+                            circumference = Convert.ToInt32(Console.ReadLine());
+                            var calculationVrouw = (lengthInCm + 4 * circumference - 100) / 2;
+                            Console.WriteLine("Het ideale lichaamsgewicht voor u is: " + calculationVrouw);
+                        }
+                        else
+                        {
+                            Console.WriteLine("U moet kiezen uit Man of Vrouw. Probeer het opnieuw: ");
+                            sex = Console.ReadLine();
+                            sex = sex.ToUpper();
+                        }
+                    }
+                    break;
+                case "ENGLISH":
+
+                    Console.WriteLine("Are you a male or a female?");
+                    sex = Console.ReadLine();
+                    sex = sex.ToUpper();
+
+                    while (acceptedSex != 1)
+                    {
+                        if (sex == "MALE")
+                        {
+                            acceptedSex++;
+                            Console.WriteLine("How tall are you? (in centimeters): ");
+                            lengthInCm = Convert.ToInt32(Console.ReadLine());
+                            var calculationMan = (lengthInCm - 100) * 0.9;
+                            Console.WriteLine("The ideal weight for you is: " + calculationMan);
+
+                        }
+                        else if (sex == "FEMALE")
+                        {
+                            acceptedSex++;
+                            Console.WriteLine("How tall are you? (in centimeters): ");
+                            lengthInCm = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("What is your wrist width?:  ");
+                            circumference = Convert.ToInt32(Console.ReadLine());
+                            var calculationVrouw = (lengthInCm + 4 * circumference - 100) / 2;
+                            Console.WriteLine("The ideal weight for you is: " + calculationVrouw);
+                        }
+                        else
+                        {
+                            Console.WriteLine("You have to choose a gender. Please try again: ");
+                            sex = Console.ReadLine();
+                            sex = sex.ToUpper();
+                        }
+                    }
+                    break;
+
+
+
+            }
         }
     }
 }
